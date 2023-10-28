915dd9975f1995a8a409800420b4e9a27ffda6ff
Annie Chakraborty <46227364+anniechakraborty@users.noreply.github.com>
2023-10-28 15:45:27 +0530

    how to clean up the api routes

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -2,15 +2,14 @@ const express = require('express');
 const router = express.Router();
 const { getAllGoals, getGoal, createGoal, updateGoal, deleteGoal } = require('../controllers/goalController');
 
+// router.get('/', getAllGoals)
+// router.get('/:id', getGoal)
+// router.post('/', createGoal)
+// router.put('/:id', updateGoal)
+// router.delete('/:id', deleteGoal)
 
-router.get('/', getAllGoals)
-
-router.get('/:id', getGoal)
-
-router.post('/', createGoal)
-
-router.put('/:id', updateGoal)
-
-router.delete('/:id', deleteGoal)
+// We can clean up the above implementation by grouping the routes like this
+router.route('/').get(getAllGoals).post(createGoal);
+router.route('/:id').get(getGoal).delete(deleteGoal).put(updateGoal);
 
 module.exports = router
