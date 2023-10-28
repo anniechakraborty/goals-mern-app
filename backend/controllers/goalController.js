f8010f220b7eb0bc04696f0b9022981a730b38fc
Annie Chakraborty <46227364+anniechakraborty@users.noreply.github.com>
2023-10-28 19:42:18 +0530

    added async handler to all the controller functions

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,21 +1,23 @@
+const asyncHandler = require('express-async-handler')
+
 // @desc    : This function gets all the goals
 // @route   : GET /api/goals 
 // @access  : Private
-const getAllGoals = (req, res)=>{
+const getAllGoals = asyncHandler(async (req, res)=>{
     res.status(200).json({message : "Get goals!"});
-}
+})
 
 // @desc    : This function gets a specific goal
 // @route   : GET /api/goals/:id 
 // @access  : Private
-const getGoal = (req, res)=>{
+const getGoal = asyncHandler(async (req, res)=>{
     res.status(200).json({message : `Get goal ${req.params.id}`});
-}
+})
 
 // @desc    : This function creates a goal
 // @route   : POST /api/goals 
 // @access  : Private
-const createGoal = (req, res)=>{
+const createGoal = asyncHandler(async (req, res)=>{
     console.log("Body :", req.body);
     if(!req.body.text){
         // // status 400 is a bad request, i.e. when there is a client error and params aren't sent correctly or are missing
@@ -26,21 +28,21 @@ const createGoal = (req, res)=>{
         throw new Error('Bad request! Please send a Text Field value!');
     }
     res.status(200).json({message : "Created goal!"});
-}
+})
 
 // @desc    : This function updates a goal
 // @route   : PUT /api/goals/:id 
 // @access  : Private
-const updateGoal =(req, res)=>{
+const updateGoal =asyncHandler(async (req, res)=>{
     res.status(200).json({message : `Updated goal ${req.params.id}`});
-}
+})
 
 // @desc    : This function deletes a goal
 // @route   : DELETE /api/goals/:id 
 // @access  : Private
-const deleteGoal = (req, res)=>{
+const deleteGoal = asyncHandler(async (req, res)=>{
     res.status(200).json({message : `Deleted goal ${req.params.id}`});
-}
+})
 
 module.exports = {
     getAllGoals,
